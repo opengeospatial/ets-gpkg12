f9912ffdee20cd2f22d78d05fc24eb70b76a1814
Jeff Yutzler <jyutzler@yahoo.com>
2018-06-22 13:37:01 -0400

    clarifying failures for one of the tests

--- src/test/java/org/opengis/cite/gpkg12/VerifyTestNGController.java
+++ src/test/java/org/opengis/cite/gpkg12/VerifyTestNGController.java
@@ -75,7 +75,7 @@ public class VerifyTestNGController {
         runTests(ClassLoader.getSystemResource("gpkg/simple_sewer_features.gpkg"), 1); // This is an invalid 1.0 or 1.1 GPKG - it has an invalid metadata table (md_standard_URI instead of md_standard_uri) 
         runTests(ClassLoader.getSystemResource("gpkg/sample1_1.gpkg"), 1); // R77
         runTests(ClassLoader.getSystemResource("gpkg/sample1_2.gpkg"), 1); // R77
-        runTests(ClassLoader.getSystemResource("gpkg/rivers-bad.gpkg"), 8); // Why did this fail again?
+        runTests(ClassLoader.getSystemResource("gpkg/rivers-bad.gpkg"), 8); // R107 to R114 (Metadata extension)
         runTests(ClassLoader.getSystemResource("gpkg/sample1_2F10.gpkg"), 2); // Default "undefined", R77
         runTests(ClassLoader.getSystemResource("gpkg/geonames_belgium.gpkg"), 5); // lower case data types R5, R77, R61, R105
 //        runTests(ClassLoader.getSystemResource("gpkg/haiti-vectors-split.gpkg"), 3); // lower case data types R5, R77 Dropping this one because it is big and doesn't offer anything new
